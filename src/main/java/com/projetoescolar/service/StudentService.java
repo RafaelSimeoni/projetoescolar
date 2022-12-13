d5be26191ce39cb69ec9c73a27d2a3e4dd1b12c8
rafael.simeoni <rafael.simeoni@foursys.com.br>
2022-12-13 11:03:11 -0300

    model mapper criado

--- src/main/java/com/projetoescolar/service/StudentService.java
+++ src/main/java/com/projetoescolar/service/StudentService.java
@@ -1,4 +1,11 @@
 package com.projetoescolar.service;
 
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+
 public class StudentService {
+
+    @Autowired
+    ModelMapper mapper;
+
 }
